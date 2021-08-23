1cb49dae284675751a467966f0a09c6ce3108d29
GayuhAbdiMahardika <50190371+GayuhAbdiMahardika@users.noreply.github.com>
2021-08-24 05:53:47 +0700

    Andoid Week 5

--- RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.java
+++ RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    SiswaAdapter adapter;
+    List<Siswa> siswaList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        load();
+        isiData();
+    }
+
+    public void load(){
+        recyclerView = findViewById(R.id.rcvSiswa);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    public void isiData(){
+        siswaList = new ArrayList<Siswa>();
+        siswaList.add(new Siswa("Padika", "Sidoarjo"));
+        siswaList.add(new Siswa("Pagayuh", "Sidoarjo"));
+        siswaList.add(new Siswa("DreamBig", "Sidoarjo"));
+        siswaList.add(new Siswa("Thor it Fly", "Sidoarjo"));
+        siswaList.add(new Siswa("SoloPlayer", "Sidoarjo"));
+
+        adapter = new SiswaAdapter(this, siswaList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    public void btnTambah(View view) {
+        siswaList.add(new Siswa("MultiPlayer", "Sidoarjo"));
+        adapter.notifyDataSetChanged();
     }
 }
